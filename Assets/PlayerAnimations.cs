52e849901bdbb93b362f495860b1d813f10c60ed
Saulo Caliman Gomes <saulocalimangomes@gmail.com>
2023-05-24 16:50:29 -0300

    Animações do player

---
+++ Assets/PlayerAnimations.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.Design.Serialization;
+
+public class PlayerAnimations
+{
+    public const string damage = "player_damage";
+    public const string shoot = "player_shoot";
+    public const string walk = "player_walk";
+    public const string jump = "player_jump";
+    public const string idle = "player_idle";
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,113 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    float groundRadius;
+    [SerializeField]
+    LayerMask groundLayerMask;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float jumpForce;
+
+    public bool onGround = false;
+
+    string currentAnimation;
+    Animator animator;
+    Rigidbody2D rb;
+
+    float hAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator= GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer) { 
+            hAxis = Input.GetAxis("Horizontal");
+            if (onGround)
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    rb.AddForce(Vector3.up * jumpForce);
+                    //rb.velocity =  * Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+        ChangeAnimation();
+        Gravity();
+    }
+
+    void Jump() 
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position,
+            groundRadius,
+            groundLayerMask);
+
+    }
+
+    void Move() 
+    {
+        rb.velocity = Vector3.right * hAxis * speed * Time.deltaTime;
+    }
+
+    void ChangeAnimation() 
+    {
+        if (Mathf.Abs(hAxis) > 0)
+        {
+            DoAnimation(PlayerAnimations.walk);
+            if (hAxis > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (hAxis < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+        else
+        {
+            DoAnimation(PlayerAnimations.idle);
+        }
+    }
+
+    void Gravity() {
+        if (!onGround) {
+            rb.AddForce(Vector2.down * 100);
+        }
+    }
+
+    void DoAnimation(string animation) 
+    {
+        if (animation == currentAnimation)
+            return;
+        currentAnimation = animation;
+        animator.Play(currentAnimation);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.position, groundRadius);
+    }
+
+}
